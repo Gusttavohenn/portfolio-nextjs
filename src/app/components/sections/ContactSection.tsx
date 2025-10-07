f14479d89dc31b2f4dde53552b211b59fc0ba93f
Gustavo Henrique de Oliveira <gustavo.oliveira@minasgusa.local>
2025-10-07 16:39:00 -0300

    feat: Adiciona campo de telefone para contato

--- src/app/components/sections/ContactSection.tsx
+++ src/app/components/sections/ContactSection.tsx
@@ -4,14 +4,13 @@ import { useEffect } from 'react';
 
 export default function ContactSection() {
 
-  // Adicionamos a lógica do formulário AQUI, dentro do próprio componente
   useEffect(() => {
     const form = document.getElementById('contact-form') as HTMLFormElement;
     const formButton = form.querySelector('.form-button') as HTMLButtonElement;
     const formButtonText = form.querySelector('.button-text') as HTMLSpanElement;
     const formStatus = document.getElementById('form-status') as HTMLParagraphElement;
 
-    if (!form) return; // Se não achar o form, para a execução
+    if (!form) return;
 
     const handleFormSubmit = (e: Event) => {
         e.preventDefault();
@@ -53,20 +52,20 @@ export default function ContactSection() {
 
     form.addEventListener('submit', handleFormSubmit);
 
-    // Função de limpeza para remover o event listener
+    // 
     return () => {
       form.removeEventListener('submit', handleFormSubmit);
     };
-  }, []); // O array vazio garante que rode apenas uma vez
+  }, []);
 
   return (
     <section id="contato" className="section">
       <h2 className="section-title">Contato</h2>
       <p>Pronto para criar algo extraordinário juntos?</p>
-      {/* Verifique se a sua URL do Formspree está correta aqui */}
       <form id="contact-form" action="https://formspree.io/f/xyzndejv" method="POST" className="contact-form">
         <input type="text" name="name" placeholder="Seu Nome" required />
         <input type="email" name="email" placeholder="Seu E-mail" required />
+        <input type="number" name="telefone" placeholder="Telefone/Whastapp | (DDD) 9999-9999" required />
         <textarea name="message" placeholder="Sua Mensagem" required></textarea>
         <button type="submit" className="cta-button form-button"><span className="button-text">ENVIAR MENSAGEM</span></button>
       </form>
